4d9746468287ad2ef23504eb91d8ca45ee1156ed
Stanislav Kad <stanislav.kad@gmail.com>
2023-11-01 21:10:42 +0100

    Add formik for login form

--- chatclient/src/components/LoginForm.tsx
+++ chatclient/src/components/LoginForm.tsx
@@ -1,88 +1,105 @@
-import { MouseEvent, useRef } from "react";
 import { Link } from "react-router-dom";
-import { UserResponse, useLoginMutation } from "../services/api";
+import { useLoginMutation } from "../services/api";
 import toast, { Toaster } from 'react-hot-toast';
-import spinner from '../assets/spinner.gif';
+import { Formik } from "formik";
+import * as Yup from 'yup';
 
 
 const LoginForm = () => {
-    const password = useRef<HTMLInputElement>(null);
-    const username = useRef<HTMLInputElement>(null);
 
-    const [login, { isLoading, data, error, status }] = useLoginMutation();
+    const validationSchema = Yup.object({
+        name: Yup.string()
+            .required("Name is required"),
+        password: Yup.string()
+            .required("Password is required")
+            .min(8, "Password must be at least 8 characters long"),
+    });
 
-    const handleSubbit = async (e: MouseEvent) => {
-        e.preventDefault();
-        console.log(password.current?.value, 'password.current?.value');
-            await login({ password: password.current?.value, username: username.current?.value })
+    const initialFieldsValues = {
+        name: "",
+        password: "",
+    };
+
+
+    const [login, { isLoading }] = useLoginMutation();
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const handleSubmit = async (values: any) => {
+
+        await login({ password: values.password, username: values.name })
             .unwrap()
-            .then((payload) => toast('fulfilled'))
+            .then(() => toast('fulfilled'))
             .catch((e) => {
                 console.log(e);
                 toast(`${e.status} ${e.data.message}`)
             })
     }
     return (
-        <div className="h-[300px] p-[20px] bg-white rounded-[10px] flex flex-col justify-between">
+        <div className="h-[320px] p-[20px] bg-white rounded-[10px] flex flex-col justify-between">
             <Toaster />
-            <input
-                type="text"
-                placeholder="Username"
-                className="h-[50px] rounded-[10px] border border-gray-400 text-lg p-[8px] font-light "
-                ref={username}
-            >
-            </input>
-            <input
-                type="password"
-                placeholder="Password"
-                className=" h-[50px] rounded-[10px] border border-gray-400 text-lg p-[8px] font-light "
-                ref={password}
-            >
-            </input>
-            {
-                isLoading ?
-                    <img className="w-[120px] h-[120px] self-center" src={spinner} alt='loading' /> : (
+            <Formik initialValues={initialFieldsValues} onSubmit={handleSubmit} validationSchema={validationSchema}>
+                {({ handleChange, handleBlur, handleSubmit, values, errors, touched }) => (
                     <>
-                        <button className="
-                            h-[50px] 
-                            rounded-[10px]
-                            bg-blue-600 w-[30%] 
-                            text-white 
-                            font-light
-                            self-center  
-                            transform 
-                            transition-transform 
-                            hover:scale-105 
-                            active:bg-blue-700 
-                            focus:outline-none
-                            "
-                            onClick={handleSubbit}
-                        >
-                            Log In
-                        </button>
-                        <span className="ml-[20%]">
-                            Forgot Password?
-                        </span>
-                        <Link to={'/register'} className="
-                            h-[50px] 
-                            rounded-[10px]
-                            bg-green-500 w-[40%] 
-                            text-white
-                            font-light
-                            self-center  
-                            transform 
-                            transition-transform 
-                            hover:scale-105 
-                            active:bg-blue-700 
-                            focus:outline-none
-                            
-                        ">
-                            <p className="flex justify-center mt-[13px]">Go to register!</p>
-                        </Link>
+                        <input
+                            type="text"
+                            placeholder="Username"
+                            className="h-[50px] rounded-[10px] border border-gray-400 text-lg p-[8px] font-light "
+                            onChange={handleChange("name")}
+                            onBlur={handleBlur("name")}
+                            value={values.name}
+                        />
+                        {touched.name && errors.name && <p className="text-red-300 ml-2">{errors.name}</p>}
+                        <input
+                            type="password"
+                            placeholder="Password"
+                            className=" h-[50px] rounded-[10px] border border-gray-400 text-lg p-[8px] font-light "
+                            onChange={handleChange("password")}
+                            onBlur={handleBlur("password")}
+                            value={values.password}
+                        />
+                        {touched.password && errors.password && <p className="text-red-300 ml-2">{errors.password}</p>}
+                        <>
+                            <button className="
+                                h-[50px] 
+                                rounded-[10px]
+                                bg-blue-600 w-[30%] 
+                                text-white 
+                                font-light
+                                self-center  
+                                transform 
+                                transition-transform 
+                                hover:scale-105 
+                                active:bg-blue-700 
+                                focus:outline-none
+                                "
+                                type="submit"
+                                onClick={handleSubmit}
+                            >
+                                {isLoading ? 'Sending' : 'Log In'}
+                            </button>
+                            <span className="ml-[20%]">
+                                Forgot Password?
+                            </span>
+                            <Link to={'/register'} className="
+                                h-[50px] 
+                                rounded-[10px]
+                                bg-green-500 w-[40%] 
+                                text-white
+                                font-light
+                                self-center  
+                                transform 
+                                transition-transform 
+                                hover:scale-105 
+                                active:bg-blue-700 
+                                focus:outline-none
+                                
+                            ">
+                                <p className="flex justify-center mt-[13px]">Go to register!</p>
+                            </Link>
+                        </>
                     </>
-                )
-            }
-
+                )}
+            </Formik>
         </div>
     )
 }
